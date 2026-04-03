e4906a07a46f115ef875308b8d1ce521c9dc16a0
Bluehawk2008 <bluehawk2008@gmail.com>
2026-04-03 01:27:49 -0400

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using CanadianLeopards;
+
+[assembly: MelonInfo(typeof(CanadianLeopardsClass), "CanadianLeopards", "1.2.0", "Bluehawk, Agentgummby")]
+[assembly: MelonGame("Radian Simulations LLC", "GHPC")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CanadianLeopards")]
+[assembly: AssemblyDescription("A cosmetic mod for GHPC that converts Bundeswehr Leopard 1s into Canadian C1s")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bluehawk & Agentgummby")]
+[assembly: AssemblyProduct("CanadianLeopards")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("beff40d2-5ccc-4703-ac3b-e0bbbce34a33")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
